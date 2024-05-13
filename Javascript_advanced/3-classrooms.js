6b9226f9d4a0cdcbc487b9f0249fe8d633b4fddf
Maxime <maxime@MacBook-Pro-de-Jacques-2.local>
2024-05-13 12:47:06 +0200

    task 3

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,16 @@
+function createClassRoom(numbersOfStudents) {
+    function studentSeat(seat) {
+        return () => seat;
+    }
+    
+    let students = [];
+
+    for (let i = 0; i < numbersOfStudents; ++i) {
+        let seatNumber = studentSeat(i + 1);
+        students.push(seatNumber);
+    }
+
+    return students;
+}
+
+let classRoom = createClassRoom(10);
